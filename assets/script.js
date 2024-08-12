ce582540de416fe931f29f54085a2b9a474b125b
Celia Haro <charo@keldin.fr>
2024-08-12 21:05:27 +0200

    JS optimisé

--- assets/script.js
+++ assets/script.js
@@ -18,84 +18,66 @@ const slides = [
   },
 ];
 
-// Gestion des flèches, vérifications
+// Définir mes variables const contexte global
 
 const leftArrow = document.querySelector("#banner .arrow_left");
 const rightArrow = document.querySelector("#banner .arrow_right");
 
+const pathImage = "./assets/images/slideshow/";
+const bannerImage = document.querySelector("#banner .banner-img");
+const bannerTagLine = document.querySelector("#banner p");
+
+// Création de la fonction de gestion de l'index
+
+let currentIndex = 0;
+
+function indexControl(direction) {
+  if (direction === "+") {
+    currentIndex += 1;
+  } else if (direction === "-") {
+    currentIndex -= 1;
+  }
+
+  if (currentIndex < 0) {
+    currentIndex = 3;
+  } else if (currentIndex > 3) {
+    currentIndex = 0;
+  }
+  return currentIndex;
+}
+
+// Gestion de l'actualisation de la slide (img.src + p innerHTML)
+
+function actualiseSlide() {
+  bannerImage.setAttribute("src", pathImage + slides[currentIndex].image);
+  bannerTagLine.innerHTML = slides[currentIndex].tagLine;
+}
+
+// Fonction de l'actualisation des dots en fonction de currentIndex
+function actualiseDot() {
+  const currentDotSlide = document.querySelector(
+    `.dots .dot:nth-child(${currentIndex + 1})`
+  );
+  const allDots = document.querySelectorAll(".dots .dot");
+
+  allDots.forEach((dot) => {
+    dot.classList.remove("dot_selected");
+  });
+
+  currentDotSlide.classList.add("dot_selected");
+}
+
+// Gestion des évènements
 leftArrow.addEventListener("click", () => {
+  currentIndex = indexControl("-");
+  actualiseSlide();
+  actualiseDot();
   console.log("J'ai cliqué sur la flèche de gauche");
 });
 
 rightArrow.addEventListener("click", () => {
+  currentIndex = indexControl("+");
+  actualiseSlide();
+  actualiseDot();
   console.log("J'ai cliqué sur la flèche de droite");
-});
-
-// Décomposition de mon tableau pour récupérer des objets
-
-const [slide1, slide2, slide3, slide4] = slides;
-
-// Définition des variables pour gérer mes slides
-
-const pathImage = "../assets/images/slideshow/";
-const bannerImage = document.querySelector("#banner .banner-img");
-const bannerTagLine = document.querySelector("#banner p");
-
-// Définition des variables pour gérer les bullet points
-
-const dotSlide1 = document.querySelector(".dots .dot:nth-child(1)");
-const dotSlide2 = document.querySelector(".dots .dot:nth-child(2)");
-const dotSlide3 = document.querySelector(".dots .dot:nth-child(3)");
-const dotSlide4 = document.querySelector(".dots .dot:nth-child(4)");
-
-// Gestion des clics sur la flèche de droite
-
-rightArrow.addEventListener("click", () => {
-  if (bannerTagLine.innerHTML === slide1.tagLine) {
-    bannerImage.setAttribute("src", pathImage + slide2.image);
-    bannerTagLine.innerHTML = slide2.tagLine;
-    dotSlide1.classList.remove("dot_selected");
-    dotSlide2.classList.add("dot_selected");
-  } else if (bannerTagLine.innerHTML === slide2.tagLine) {
-    bannerImage.src = pathImage + slide3.image;
-    bannerTagLine.innerHTML = slide3.tagLine;
-    dotSlide2.classList.remove("dot_selected");
-    dotSlide3.classList.add("dot_selected");
-  } else if (bannerTagLine.innerHTML === slide3.tagLine) {
-    bannerImage.src = pathImage + slide4.image;
-    bannerTagLine.innerHTML = slide4.tagLine;
-    dotSlide3.classList.remove("dot_selected");
-    dotSlide4.classList.add("dot_selected");
-  } else if (bannerTagLine.innerHTML === slide4.tagLine) {
-    bannerImage.src = pathImage + slide1.image;
-    bannerTagLine.innerHTML = slide1.tagLine;
-    dotSlide4.classList.remove("dot_selected");
-    dotSlide1.classList.add("dot_selected");
-  }
-});
-
-// Gestion des clics sur la flèche de gauche
-
-leftArrow.addEventListener("click", () => {
-  if (bannerTagLine.innerHTML === slide4.tagLine) {
-    bannerImage.setAttribute("src", pathImage + slide3.image);
-    bannerTagLine.innerHTML = slide3.tagLine;
-    dotSlide4.classList.remove("dot_selected");
-    dotSlide3.classList.add("dot_selected");
-  } else if (bannerTagLine.innerHTML === slide3.tagLine) {
-    bannerImage.src = pathImage + slide2.image;
-    bannerTagLine.innerHTML = slide2.tagLine;
-    dotSlide3.classList.remove("dot_selected");
-    dotSlide2.classList.add("dot_selected");
-  } else if (bannerTagLine.innerHTML === slide2.tagLine) {
-    bannerImage.src = pathImage + slide1.image;
-    bannerTagLine.innerHTML = slide1.tagLine;
-    dotSlide2.classList.remove("dot_selected");
-    dotSlide1.classList.add("dot_selected");
-  } else if (bannerTagLine.innerHTML === slide1.tagLine) {
-    bannerImage.src = pathImage + slide4.image;
-    bannerTagLine.innerHTML = slide4.tagLine;
-    dotSlide1.classList.remove("dot_selected");
-    dotSlide4.classList.add("dot_selected");
-  }
 });
